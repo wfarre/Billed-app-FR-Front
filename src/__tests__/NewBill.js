23b9822d931bd1642dcaa94792fd33e8566d84f4
william.farre@gmail.com <william.farre@gmail.com>
2022-05-30 11:38:54 +0800

    bills coverage

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,13 +5,36 @@
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import router from "../app/Router.js";
+import { ROUTES_PATH} from "../constants/routes.js";
+import '@testing-library/jest-dom'
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store"
+
+
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+    test("Then the mail icon in the vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+
+      // await waitFor(() => screen.getByTestId('icon-mail'))
+      const mailIcon = screen.getByTestId('icon-mail')
+      //to-do write expect expression
+      expect(mailIcon.classList).toContain(`active-icon`);
+
+
+      // const html = NewBillUI()
+      // document.body.innerHTML = html
       //to-do write assertion
     })
   })
